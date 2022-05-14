9a2a4410857676dec282bcf0064bc950b57a5d70
Deknoinarak <chitah09@gmail.com>
2022-05-14 12:31:28 +0000

    first commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit8fd1efe18a28ea22e046d590212db642
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit8fd1efe18a28ea22e046d590212db642', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit8fd1efe18a28ea22e046d590212db642', 'loadClassLoader'));
